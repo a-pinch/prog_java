0908c0df5f881f739549696b46edaddb63929196
Andrei Pinchuk <andrei.pinchuk@altoros.com>
2016-07-03 13:24:47 +0300

    convert end string format from real System.in to System.out

--- src/org/stepic/pinch/s52_9/Main.java
+++ src/org/stepic/pinch/s52_9/Main.java
@@ -1,26 +1,18 @@
 package org.stepic.pinch.s52_9;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        InputStream stdin = System.in;
-        try {
-            System.setIn(new ByteArrayInputStream(new byte[]{65,13,10,10,13}));
             int a, b = System.in.read();
             if(b == -1) return;
             while ((a = System.in.read()) > 0) {
                 if (a != 10 || b != 13) {
-                    System.out.write((""+b+" ").getBytes());
+                    System.out.write(b & 0xFF);
                 }
                 b = a;
             }
-            System.out.write((""+b+" ").getBytes());
+            System.out.write(b & 0xFF);
             System.out.flush();
-        }finally {
-            System.setIn(stdin);
-        }
     }
 }
